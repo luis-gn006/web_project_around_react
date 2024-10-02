161996368066f735e544be50b9a7e01c224f4045
lapLGH <luis-enrique06@outlook.com>
2024-10-02 14:16:14 -0600

    refactorize api

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Tripleten web_project_around
 
-# Proyecto realizado en el bootcamp de desarrollo web de Tripleten. Realizado con HTML, CSS, JS y React. El proyecto fue realizado en VSS (Visual Studio Code)
+# Proyecto realizado en el bootcamp de desarrollo web de Tripleten. Utiliza HTML, CSS, JS y React. El proyecto fue realizado en VSS (Visual Studio Code).
 
 ## ¿De qué se trata?
 
-### Se realizó una página interactiva con semejanza a una red social. El usuario puede añadir, eliminar o dar like a las fotos, al igual que editar su nombre de usuario e información acerca de sí mismo, todo esto a través de una API.
+### Se creó una página interactiva similar a una red social donde los usuarios pueden añadir, eliminar y dar like a fotos, así como editar su nombre de usuario e información personal a través de una API.
 
-## El proyecto se adapta en las siguientes resoluciones:
+## El proyecto se adapta a las siguientes resoluciones:
 
 1. 1280px Desktop
 
@@ -14,16 +14,44 @@
 
 3. 320px Mobile
 
-## Preview del Proyecto:
+## Preview del proyecto:
 
 ![alt text](./src/images/preview__projecto-around.png)
 
 ## Aprendizaje:
 
-### Estes es mi primer poryecto usando JS en el cual se empleo POO, asincronia con conexion a una API. Tambien se uso Webpack para el empaquetado del proeycto. Hubo muchos procesos de refactorizacion para poner en practica clases y practivas más avanzadas de JS.El nuevo reto fue realizar todo esto en React.
+### Este es mi primer proyecto usando React, en el cual se empleó el uso de hooks para la funcionalidad de la página, además de asincronía con conexión a una API.
 
-## Link de Github pages para visualizar mi pagina: https://luis-gn006.github.io/web_project_around_react
+## Detalles del código
+
+### El proyecto está estructurado en componentes de React. A continuación, se describen las partes más relevantes:
+
+### Estructura de componentes:
+
+#### App: Componente principal que gestiona el estado de la aplicación y la lógica de los popups.
+
+#### Header: Componente que muestra el encabezado de la aplicación.
+
+#### Main: Componente que presenta las tarjetas de fotos y la lógica de interacción.
+
+#### Footer: Componente que muestra el pie de página.
+
+#### Popups: Componentes modales para editar perfil, añadir nuevas fotos, editar avatar y confirmar la eliminación de tarjetas.
+
+#### Gestión del Estado:
+
+### Utilizo el hook useState de React para manejar el estado de la información del usuario, las tarjetas y la apertura/cierre de popups.
+
+### La información del usuario se carga de manera asincrónica utilizando api.getUserInfo(), y las tarjetas se gestionan a través de api.getInitialCards().
+
+### Interacciones:
+
+### Funciones para añadir, eliminar y dar "like" a las tarjetas, utilizando métodos de la API.
+
+### Validaciones en los formularios de entrada para asegurar que los datos sean correctos antes de enviarlos a la API.
+
+## Link de Github pages para visualizar mi página: https://luis-gn006.github.io/web_project_around_react
 
 # © Luis Gonzalez
 
-# Espero que disfruten de este proyecto tanto como disfrute haciéndolo. ¡Saludos!
+# Espero que disfruten de este proyecto tanto como disfruté haciéndolo. ¡Saludos!
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,146 +1,61 @@
 class Api {
-  constructor(url, authorization, makeRequest) {
-    this.url = url;
-    this.authorization = authorization;
-    this.makeRequest = makeRequest;
+  constructor(url, token) {
+    this._url = url;
+    this._token = token;
+  }
+  _makeRequest(uri, method = "GET", body = null) {
+    const options = {
+      headers: {
+        "Content-Type": "application/json",
+        authorization: this._token,
+      },
+      method,
+    };
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
+    const request = fetch(`${this._url}/${uri}`, options);
+    return request
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(`Error: ${response.status}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
   getUserInfo() {
-    return fetch(`${this.url}/users/me`, {
-      headers: {
-        authorization: this.authorization,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(`Error: ${response.status}`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    return this._makeRequest("users/me");
   }
   getInitialCards() {
-    return fetch(`${this.url}/cards`, {
-      headers: {
-        authorization: this.authorization,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(`Error: ${response.status}`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    return this._makeRequest("cards");
   }
   patchUserInfo(name, about) {
-    return fetch(`${this.url}/users/me`, {
-      method: "PATCH",
-      headers: {
-        authorization: this.authorization,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        about: about,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(`Error: ${response.status}`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    return this._makeRequest("users/me", "PATCH", { name, about });
   }
   postNewCard(name, link) {
-    return fetch(`${this.url}/cards`, {
-      headers: {
-        authorization: this.authorization,
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify({
-        name,
-        link,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(`Error: ${response.status}`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    return this._makeRequest("cards", "POST", { name, link });
   }
   deleteCard(cardId) {
-    return fetch(`${this.url}/cards/${cardId}`, {
-      method: "DELETE",
-      headers: {
-        authorization: this.authorization,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(`Error: ${response.status}`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    return this._makeRequest(`cards/${cardId}`, "DELETE", { cardId });
   }
-  patchUserAvatar(avatarLink) {
-    return fetch(`${this.url}/users/me/avatar`, {
-      method: "PATCH",
-      headers: {
-        authorization: this.authorization,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        avatar: avatarLink,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(`Error: ${response.status}`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  patchUserAvatar(avatar) {
+    return this._makeRequest("users/me/avatar", "PATCH", { avatar });
   }
   changeLikeCardStatus(cardId, isLiked) {
-    return fetch(`${this.url}/cards/likes/${cardId}`, {
-      method: `${isLiked ? "PUT" : "DELETE"}`,
-      headers: {
-        authorization: this.authorization,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(`Error: ${response.status}`);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    return this._makeRequest(
+      `cards/likes/${cardId}`,
+      `${isLiked ? "PUT" : "DELETE"}`,
+      { cardId }
+    );
   }
 }
 
 const api = new Api(
   "https://around.nomoreparties.co/v1/web_es_10",
-  "541d0e53-114b-4fb1-9af0-b09c04c191b9",
-  ""
+  "541d0e53-114b-4fb1-9af0-b09c04c191b9"
 );
 
 export default api;
